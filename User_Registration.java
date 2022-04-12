c03a6a3095deb1ab9590bef4c6baf4196ed98b0c
Karthik <jeefkarthik036@gmail.com>
2022-04-12 23:54:29 +0530

    added-UC4

--- User_Registration.java
+++ User_Registration.java
@@ -35,9 +35,20 @@ public class User_Registration {
 	        else
 	            System.out.println("Invalid Email");
 	    }
+	    
+	    public static void mobile() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("\nEnter Mobile Number With Country Code(space between code and number)");
+	        String m = sc.nextLine();
+	        if (Pattern.matches("^[1-9]{2}\\s{1}[1-9]{1}[0-9]{9}$", m) == true)
+	            System.out.println("Valid Mobile");
+	        else
+	            System.out.println("Invalid Mobile");
+	    }
 	    public static void main(String[] args) {
 	        firstName();
 	        lastName();
 	        email();
+	        mobile();
 	    }
 	}
