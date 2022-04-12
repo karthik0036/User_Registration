0db154e42be2bca8c09bb6832e94183e622eb8a8
Karthik <jeefkarthik036@gmail.com>
2022-04-13 00:15:06 +0530

    added-UC7

--- User_Registration.java
+++ User_Registration.java
@@ -45,12 +45,13 @@ public class User_Registration {
 	        else
 	            System.out.println("Invalid Mobile");
 	    }
-	 // Rule2 Should have one upper case
+	 // Rule3 Should have atleast 1 numeric character
 	    public static void password() {
 	        Scanner sc  = new Scanner(System.in);
 	        System.out.println("\nEnter the Password");
 	        String p = sc.nextLine();
-	        if (Pattern.matches("^(?=.*[a-z])\"+\"(?=.*[A-Z]){8,}$", p) == true)//should have one upper case
+	        if (Pattern.matches("^(?=.*[0-9])\"+\"(?=.*[a-z])\"+\"(?=.*[A-Z])\"+\"(?=.*[@#$%^&+=]){8,}$", p) == true)
+	        	
 	            System.out.println("Valid Password");
 	        else
 	            System.out.println("Invalid Password");
