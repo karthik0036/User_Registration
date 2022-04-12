b24ec3c4fae87041269bfa3731f1f9aa575d4891
Karthik <jeefkarthik036@gmail.com>
2022-04-12 23:30:14 +0530

    added-UC1

---
+++ User_Registration.java
@@ -0,0 +1,22 @@
+package com.bridge;
+
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class User_Registration {
+	 static Scanner sc = new Scanner(System.in);
+
+
+	    public static void firstName() {
+	        System.out.println("Enter First Name");
+	        String fN = sc.nextLine();
+	        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fN) == true)
+	            System.out.println("Valid First Name");
+	        else
+	            System.out.println("Invalid First Name");
+	    }
+	    public static void main(String[] args) {
+	        firstName();
+	    }
+	}
