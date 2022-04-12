fdaa6b3c7c2937719f8b38d7fea2995608455471
Karthik <jeefkarthik036@gmail.com>
2022-04-12 23:34:20 +0530

    added-UC2

--- User_Registration.java
+++ User_Registration.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 public class User_Registration {
 	 static Scanner sc = new Scanner(System.in);
 
-
 	    public static void firstName() {
 	        System.out.println("Enter First Name");
 	        String fN = sc.nextLine();
@@ -16,7 +15,19 @@ public class User_Registration {
 	        else
 	            System.out.println("Invalid First Name");
 	    }
+
+	    
+	    public static void lastName() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("\nEnter Last Name");
+	        String lN = sc.nextLine();
+	        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
+	            System.out.println("Valid Last Name");
+	        else
+	            System.out.println("Invalid Last Name");
+	    }
 	    public static void main(String[] args) {
 	        firstName();
+	        lastName();
 	    }
 	}
